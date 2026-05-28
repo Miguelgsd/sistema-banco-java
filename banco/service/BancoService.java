a137d0faa5e0ded02019abdffd325cff3cd00b8b
Miguelgsd <miguelcbdutra@gmail.com>
2026-05-28 16:03:55 -0300

    Estrutura inicial do BancoService

--- banco/service/BancoService.java
+++ banco/service/BancoService.java
@@ -1,1 +1,22 @@
 package banco.service;
+
+import java.util.List;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class BancoService{
+    private List<ContaCorrente> contasCorrentes;
+    private List<ContaPoupanca> contasPoupanca;
+
+    public void cadastrarContaCorrente(ContaCorrente cc){
+        this.contasCorrentes.add(cc);
+    }
+
+    public void cadastrarContaPoupanca(ContaPoupanca cp){
+        this.contasPoupanca.add(cp);
+    }
+
+    public ContaBancaria buscarConta(String numeroConta){
+        
+    }
+}
