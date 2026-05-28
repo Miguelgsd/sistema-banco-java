55adfeea74b819fa1db5fffdf41bcbca972c6f02
Miguelgsd <miguelcbdutra@gmail.com>
2026-05-28 15:01:17 -0300

    Inclusão dos demais métodos - sem correção de bugs

--- banco/model/ContaPoupanca.java
+++ banco/model/ContaPoupanca.java
@@ -5,4 +5,18 @@ public class ContaPoupanca extends ContaBancaria {
         super(numeroConta, titular, saldoInicial);
         this.taxaRendimentoMensal = taxa;
     }
+
+    public double calcularRendimento(){
+        return this.saldo * this.taxaRendimentoMensal;
+    }
+
+    public void aplicarRendimento(){
+        this.saldo += this.saldo * this.taxaRendimentoMensal;
+        this.historico.add("Rendimento aplicado ao saldo.");
+    }
+
+    @Override
+    abstract void gerarExtrato(){
+        JOptionPane.showMessageDialog(null, "--------- Extrato ----------\n\n" + "Titular: " + getTitular() + "\nNúmero: " + getNumeroConta() + "\nSaldo" + getSaldo() + "\nTaxa de rendimento: " + this.taxaRendimentoMensal + "\nRendimento estimado: " + "\nHistórico: \n" + getHistorico());
+    }
 }
